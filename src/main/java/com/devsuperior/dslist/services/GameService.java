0244ce68fab8d4b45323d447f7fcf6afb12c892f
talita <talitamsx@gmail.com>
2025-08-19 22:09:07 -0300

    GET game

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,7 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
     //Service devolve uma lista de DTO
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         //  var result = gameRepository.findAll(); Isso é a mesma coisa que List<Game> result = gameRepository.findAll();
         List<Game> result = gameRepository.findAll();
