7b7af47a7ef0a8e614844fedb15b0db4ac2562c4
talita <talitamsx@gmail.com>
2025-08-19 22:50:33 -0300

    GET games by list

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,13 @@ public class GameService {
     public List<GameMinDTO> findAll() {
         //  var result = gameRepository.findAll(); Isso é a mesma coisa que List<Game> result = gameRepository.findAll();
         List<Game> result = gameRepository.findAll();
+        return result.stream()
+                .map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream()
                 .map(x -> new GameMinDTO(x)).toList();
     }
